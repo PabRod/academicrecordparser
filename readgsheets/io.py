d2b20ccce5c905b2b2f9f0f789e206edd9116006
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2024-09-30 14:03:55 +0200

    Hello world!

---
+++ readgsheets/io.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def _default_key():
+    """ Return the default key """
+    with open("key.txt") as f:
+        key = f.readline()
+        
+        return(key)
+
+def parse(key=_default_key()):
+    """ Return the raw data """
+    link = f"https://docs.google.com/spreadsheets/export?id={key}&format=csv"
+    data = pd.read_csv(link)
+
+    return data
+
